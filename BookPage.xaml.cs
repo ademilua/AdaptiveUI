dca6dca877cd998d3e90df009f0c41aa8a00e49a
Tolulope <tolesy2003@outlook.com>
2018-03-06 15:13:22 +0200

    Add files via upload

---
+++ BookPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using AboutMeToluUsingAdaptiveUi.Models;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AboutMeToluUsingAdaptiveUi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BookPage : Page
+
+    {
+        private List<Book> Books;
+        public BookPage()
+        {
+            this.InitializeComponent();
+            Books = BookManager.GetBook();
+        }
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var book = (Book)e.ClickedItem;
+            ResultTextBlock.Text = "You selected " + book.BookTittle  +  book.BookAuthor;
+        }
+    }
+}
